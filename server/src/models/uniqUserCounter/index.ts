9084e8ca405e62b9b35aa60bf6a7ad92f063c128
Justin Starks <jus2793@cornell.edu>
2023-01-16 01:12:12 -0500

    Change incrementCount

--- server/src/models/uniqUserCounter/index.ts
+++ server/src/models/uniqUserCounter/index.ts
@@ -1,11 +1,16 @@
 import { Schema, model, Document } from "mongoose";
-import User from "../user";
 
 interface UniqueUserCounterInterface {
+  date: Date;
   count: number;
 }
 
 const UniqueUserCounterSchema = new Schema<UniqueUserCounterInterface>({
+  date: {
+    type: Date,
+    required: true,
+    default: new Date(new Date().toISOString().slice(0, 10)),
+  },
   count: { type: Number, required: true, unique: true },
 });
 
@@ -13,15 +18,16 @@ type UniqueUserCounterModel = UniqueUserCounter &
   Document<UniqueUserCounterInterface>;
 
 const UniqueUserCounterModel = model<UniqueUserCounterInterface>(
-  "UniqueUserCounter",
+  "UniqueUser",
   UniqueUserCounterSchema
 );
 
 export default class UniqueUserCounter {
   static async incrementCount() {
-    const model = await UniqueUserCounterModel.findOne();
+    const currentDate = new Date().toISOString().slice(0, 10);
+    const model = await UniqueUserCounterModel.findOne({ date: currentDate });
     if (!model) {
-      return UniqueUserCounterModel.create({ count: 1 });
+      return UniqueUserCounterModel.create({ date: currentDate, count: 1 });
     }
     model.count += 1;
     return model.save();
