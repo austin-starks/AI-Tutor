a2ecae6ba6560aa298f4d78253b52ca5066ccc7d
Justin Starks <jus2793@cornell.edu>
2023-01-21 14:07:29 -0500

    Do better error handling for controller

--- server/src/controller/eventController.ts
+++ server/src/controller/eventController.ts
@@ -39,19 +39,24 @@ class UserController {
     res: Response,
     next: NextFunction
   ) => {
-    if (req.cookies.analaytics_uniqSubmitRequests) {
-      return next();
+    try {
+      if (req.cookies.analaytics_uniqSubmitRequests) {
+        return next();
+      }
+      // generate a random string
+      const uniqId =
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15);
+      res.cookie(
+        "analaytics_uniqSubmitRequests",
+        req.cookies.analaytics_uniqId || uniqId
+      );
+      await SubmitRequestsCounter.incrementCount();
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ msg: "Internal server error" });
     }
-    // generate a random string
-    const uniqId =
-      Math.random().toString(36).substring(2, 15) +
-      Math.random().toString(36).substring(2, 15);
-    res.cookie(
-      "analaytics_uniqSubmitRequests",
-      req.cookies.analaytics_uniqId || uniqId
-    );
-    await SubmitRequestsCounter.incrementCount();
-    next();
   };
 }
 
